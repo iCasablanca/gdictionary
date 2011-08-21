82bddb863c9067f8d4ab4d5f51324886d7c13617
Edward Shaw <easisee@gmail.com>
2011-08-21 04:01:49 -0400

    c12

--- define.php
+++ define.php
@@ -46,19 +46,7 @@ function convert($data, $type = 'container'){
 <title><?php echo $word.' &lsaquo; '.$l['text'];?></title>
 <link rel="stylesheet" type="text/css" href="/define.css" />
 <link rel="stylesheet" type="text/css" href="/search.css" />
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-18947047-2']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
+<?php if(file_exists('analytics')) {require('analytics');}?>
 </head>
 <body>
 <?php
